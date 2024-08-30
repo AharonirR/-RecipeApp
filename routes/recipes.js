ed445e28becf1d8017f388c6773219d9b3d24199
AharonirR <101107615+AharonirR@users.noreply.github.com>
2024-08-30 18:03:57 +0300

    Delete routes directory

--- routes/recipes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { searchRecipesByIngredients, searchRecipesWithFilters } = require('../controllers/recipesController');
-
-// נתיבים עבור חיפוש מתכונים
-router.get('/search-by-ingredients', searchRecipesByIngredients); // חיפוש לפי רכיבים
-router.get('/search-with-filters', searchRecipesWithFilters); // חיפוש עם סינון מתקדם
-
-module.exports = router;
