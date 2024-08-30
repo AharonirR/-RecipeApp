9f5c4087648a9afc9bdadbfadda919952c6a9ed4
AharonirR <101107615+AharonirR@users.noreply.github.com>
2024-08-30 18:12:10 +0300

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'your-username',
+  host: 'localhost',
+  database: 'your-database',
+  password: 'your-password',
+  port: 5432,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
