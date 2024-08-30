dcc0be6e6c0f22dd47675833e81c10279e3bdce6
AharonirR <101107615+AharonirR@users.noreply.github.com>
2024-08-30 17:49:49 +0300

    Update recipes.js

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { searchRecipesByIngredients } = require('../controllers/recipesController');
+const { searchRecipesByIngredients, searchRecipesWithFilters } = require('../controllers/recipesController');
 
-router.get('/search', searchRecipesByIngredients);
+// נתיבים עבור חיפוש מתכונים
+router.get('/search-by-ingredients', searchRecipesByIngredients); // חיפוש לפי רכיבים
+router.get('/search-with-filters', searchRecipesWithFilters); // חיפוש עם סינון מתקדם
 
 module.exports = router;
